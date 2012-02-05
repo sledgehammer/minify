58a03e65cdd0c5499741db08b50e48315b5d8619
Bob Fanger <bfanger@gmail.com>
2012-02-05 16:51:38 +0100

    Removed utility

--- utils/getUtils.php
+++ utils/getUtils.php
@@ -1,5 +1,4 @@
 <?php
-return  array(
-	'minify_Docroot.html' => new SledgeHammer\UtilScript('minify_DocumentRoot.php', 'Minify public javascript files'),
-);
+	// Running  "Generate static public/ folder" will also run minify_DocumentRoot.php
+	return array();
 ?>
